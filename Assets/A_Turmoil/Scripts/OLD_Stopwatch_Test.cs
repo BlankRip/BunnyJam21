8c71fadb8c39f543879797366a1c370842451a01
turmoil2k <juma1112009@hotmail.com>
2021-05-20 22:13:30 +0400

    comitting2pull

--- Assets/A_Turmoil/Scripts/OLD_Stopwatch_Test.cs
+++ Assets/A_Turmoil/Scripts/OLD_Stopwatch_Test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stopwatch_Test : MonoBehaviour
+public class OLD_Stopwatch_Test : MonoBehaviour
 {
     bool isFreezing;
 
