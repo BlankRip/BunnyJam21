ac0a127a192faad0932621d9d8ec99c447595ab7
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2021-05-21 20:26:46 +0400

    all enemy animations done

--- Assets/A_RVD/Scripts/States/Idle.cs
+++ Assets/A_RVD/Scripts/States/Idle.cs
@@ -8,22 +8,21 @@ public class Idle : AI_State
     public override void InitilizeState(AI ai) {
         if(ai.waypoints.Length > 1) {
             if(Random.Range(0, 101) > 85) {
-                //ai.myAnimator.SetBool("idle", true);
+                ai.myAnimator.SetBool("idle", true);
                 idelingFor = 0;
             }
             else
                 ai.SwitchState(connections[0]);
         } else {
-            //ai.myAnimator.SetBool("idle", true);
+            ai.myAnimator.SetBool("idle", true);
         }
-        ai.myAnimator.SetBool("idle", true);
     }
 
     public override void Exicute(AI ai) {
         if(ai.waypoints.Length > 1) {
             idelingFor += Time.deltaTime;
             if(idelingFor >= ai.idleTime) {
-                //ai.myAnimator.SetBool("idle", false);
+                ai.myAnimator.SetBool("idle", false);
                 ai.SwitchState(connections[0]);
             }
         }
