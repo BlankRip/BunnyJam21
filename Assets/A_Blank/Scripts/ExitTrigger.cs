59d985357128d31731bfa18be91a105f7023af99
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2021-05-23 06:47:04 +0400

    drive sound added

--- Assets/A_Blank/Scripts/ExitTrigger.cs
+++ Assets/A_Blank/Scripts/ExitTrigger.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ExitTrigger : MonoBehaviour
 {
+    [SerializeField] AudioClip drive;
+
     private void OnTriggerEnter(Collider other) {
         if(GameManager.instance.readyToLeave && other.CompareTag("Player")) {
             GameManager.instance.playerScript.LockMovement();
             UIManager.instance.ShowVictory();
+            GameAudio.instance.PlaySFxOneShot(drive, MixerDataBase.instance.sfxNormal);
         }
     }
 }
